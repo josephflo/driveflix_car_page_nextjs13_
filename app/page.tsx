d6924ca66ede7846032555568d1d981089c8db78
Joseph Flores <105085111+josephflo@users.noreply.github.com>
2023-11-30 14:45:40 -0300

    fix/searchmanufacturer bug

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home({ searchParams }: IHomeProps) {
       <div className="mt-12 padding-x padding-y max-width" id="explore">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <p>Choose the manufacturer and model, then search.</p>
         </div>
 
         <div className="home__filters">
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -51,21 +51,31 @@ const SearchManufacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options>
-              {
+            <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              static>
+             {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className='search-manufacturer__option'
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
                 filteredManufacturers.map((item) => (
                   <Combobox.Option
-                  key={item}
-                  className={({active}) =>`relative search-manufacturer__option ${active ? 'bg-primary-blue text-white': 'text-gray-900'}`}
-                  value={item}
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
                   >
                     {({ selected, active }) => (
                       <>
                         <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
                           {item}
                         </span>
-
-                        {/* Show an active blue background color if the option is selected */}
                         {selected ? (
                           <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
                           ></span>
@@ -74,7 +84,7 @@ const SearchManufacturer = ({
                     )}
                   </Combobox.Option>
                 ))
-              }
+              )}
             </Combobox.Options>
           </Transition>
         </div>
